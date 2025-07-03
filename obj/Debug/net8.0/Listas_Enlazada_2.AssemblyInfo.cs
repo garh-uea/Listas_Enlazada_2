e19966af871e9b857705c7f4c08884a6245468ff
Gustavo Rodríguez <ga.rodriguezh@uea.edu.ec>
2025-07-02 22:13:43 -0500

    Cambio de nombre al Programa .cs

--- obj/Debug/net8.0/Listas_Enlazada_2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Listas_Enlazada_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Listas_Enlazada_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11615405e2e820aae4e15ec95cbd0423bc894027")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c140830e2e49a127e62e206a5299511f1b4fc4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Listas_Enlazada_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Listas_Enlazada_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
